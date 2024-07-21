b63e0eb77a9b537da1fbb404233a301cf9a1ad1c
Andrii Liashenko <a.liashenko@e-u.edu.ua>
2024-07-21 21:01:22 +0300

    update create message endpoint

--- src/main/java/com/gmail/smaglenko/talkandtravel/controller/websocket/GroupMessageWebSocketController.java
+++ src/main/java/com/gmail/smaglenko/talkandtravel/controller/websocket/GroupMessageWebSocketController.java
@@ -5,23 +5,27 @@ import com.gmail.smaglenko.talkandtravel.model.dto.GroupMessageRequestDto;
 import com.gmail.smaglenko.talkandtravel.service.GroupMessageService;
 import com.gmail.smaglenko.talkandtravel.util.mapper.GroupMessageDtoMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Log4j2
 public class GroupMessageWebSocketController {
     private final GroupMessageService groupMessageService;
     private final GroupMessageDtoMapper groupMessageDtoMapper;
+    private final SimpMessagingTemplate simpMessagingTemplate;
 
-    @MessageMapping("/group-messages/{country-name}")
-    @SendTo("/countries/{countryName}")
-    public ResponseEntity<GroupMessageDto> create(@Payload GroupMessageRequestDto groupMessageRequestDto) {
+    @MessageMapping("/group-messages")
+    public void create(@Payload GroupMessageRequestDto groupMessageRequestDto) {
+        log.info("create a new message {}", groupMessageRequestDto);
         var groupMessage = groupMessageService.create(groupMessageRequestDto);
         var groupMessageDto = groupMessageDtoMapper.mapToDto(groupMessage);
-        return ResponseEntity.ok().body(groupMessageDto);
+        simpMessagingTemplate.convertAndSend("/countries/" + groupMessageRequestDto.getCountryId() + "/messages", groupMessageDto);
     }
 }
