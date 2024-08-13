7c2850be15da1a6f2318664dd42f5001daf07830
Oleksandr Prokopenko <oleksandr.prokopenko@student.itmont.de>
2024-08-13 13:14:57 +0300

    fixed exception handling bug via authentication filtering

---
+++ src/main/java/online/talkandtravel/model/dto/user/UserNameDto.java
@@ -0,0 +1,9 @@
+package online.talkandtravel.model.dto.user;
+
+/**
+ * Data transfer object representing a user's name.
+ *
+ * @param id The unique identifier of the user.
+ * @param userName The name of the user.
+ */
+public record UserNameDto(Long id, String userName) {}
