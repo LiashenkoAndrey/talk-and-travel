c216b9594547a4d981595d431211a2302cc8f7fc
Oleksandr Prokopenko <oleksandr.prokopenko@student.itmont.de>
2024-08-14 18:10:48 +0300

    sendMessage returns MessageDto with username

--- src/main/java/online/talkandtravel/controller/websocket/MessageController.java
+++ src/main/java/online/talkandtravel/controller/websocket/MessageController.java
@@ -2,7 +2,7 @@ package online.talkandtravel.controller.websocket;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import online.talkandtravel.model.dto.message.MessageDtoBasic;
+import online.talkandtravel.model.dto.message.MessageDto;
 import online.talkandtravel.model.dto.message.SendMessageRequest;
 import online.talkandtravel.service.MessageService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -14,10 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
  * Controller class responsible for handling WebSocket messages related to chat functionalities.
  *
  * <ul>
- *   <li>{@code sendMessage} - Processes a request to send a message in a chat, saves the message, and broadcasts it to relevant subscribers.</li>
+ *   <li>{@code sendMessage} - Processes a request to send a message in a chat, saves the message,
+ *       and broadcasts it to relevant subscribers.
  * </ul>
  */
-
 @RestController
 @RequiredArgsConstructor
 @Log4j2
@@ -29,7 +29,7 @@ public class MessageController {
   @MessageMapping("/messages")
   public void sendMessage(@Payload SendMessageRequest request) {
     log.info("create a new message {}", request);
-    MessageDtoBasic message = messageService.saveMessage(request);
+    MessageDto message = messageService.saveMessage(request);
 
     messagingTemplate.convertAndSend("/countries/" + request.chatId() + "/messages", message);
   }
