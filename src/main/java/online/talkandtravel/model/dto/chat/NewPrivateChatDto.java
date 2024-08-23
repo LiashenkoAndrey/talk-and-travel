ca736fa04735289616ff4d41d58877aa666a8630
LiashenkoAndrey <a.liashenko@e-u.edu.ua>
2024-08-23 21:19:59 +0300

    restore record

--- src/main/java/online/talkandtravel/model/dto/chat/NewPrivateChatDto.java
+++ src/main/java/online/talkandtravel/model/dto/chat/NewPrivateChatDto.java
@@ -1,9 +1,8 @@
 package online.talkandtravel.model.dto.chat;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 
 public record NewPrivateChatDto(
-        Long userId,
-        @NotNull @Positive Long companionId
+    @NotNull Long userId,
+    @NotNull Long companionId
 ) {}
