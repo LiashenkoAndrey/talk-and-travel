f902e14107158f1a5f719c1e2786625f68275eae
LiashenkoAndrey <a.liashenko@e-u.edu.ua>
2024-09-24 15:17:22 +0300

    add tests 2

--- src/test/java/online/talkandtravel/controller/http/integrationtest/ChatControllerIntegrationTest.java
+++ src/test/java/online/talkandtravel/controller/http/integrationtest/ChatControllerIntegrationTest.java
@@ -5,12 +5,16 @@ import static online.talkandtravel.config.TestDataConstant.PRIVATE_CHATS_DATA_SQ
 import static online.talkandtravel.config.TestDataConstant.USERS_DATA_SQL;
 import static online.talkandtravel.testdata.UserTestData.getAlice;
 import static online.talkandtravel.testdata.UserTestData.getBob;
+import static online.talkandtravel.testdata.UserTestData.getTomas;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.stream.Stream;
 import lombok.extern.log4j.Log4j2;
 import online.talkandtravel.config.IntegrationTest;
 import online.talkandtravel.exception.user.UserChatNotFoundException;
@@ -27,11 +31,9 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.stream.Stream;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 @Sql({USERS_DATA_SQL, PRIVATE_CHATS_DATA_SQL, CHAT_MESSAGES_DATA_SQL})
 @Log4j2
@@ -58,71 +60,85 @@ class ChatControllerIntegrationTest extends IntegrationTest {
   @Nested
   class CreatePrivateChat {
 
+    private static final String CREATE_PRIVATE_CHAT_PATH = "/api/chats/private";
+
+    @Test
+    void createPrivateChat_shouldThrowConflict() throws Exception {
+      performChatCreationByAlice(bob,
+          status().isConflict());
+    }
+
     @Test
     void createPrivateChat_shouldReturnChatId_whenRequestIsValid() throws Exception {
-      NewPrivateChatDto newPrivateChatDto = new NewPrivateChatDto(bob.getId());
-      String aliceToken = authenticateUser(alice);
-
-      mockMvc.perform(
-                      post("/api/chats/private")
-                              .headers(getAuthHeader(aliceToken))
-                              .contentType(APPLICATION_JSON)
-                              .content(toJson(newPrivateChatDto)))
-              .andExpect(status().isOk())
-              .andExpect(jsonPath("$").isNumber());
+      performChatCreationByAlice(getTomas(),
+          status().isOk(),
+          jsonPath("$").isNumber());
     }
 
     @Test
     void createPrivateChat_shouldThrow_whenTheSameUser() throws Exception {
-      NewPrivateChatDto newPrivateChatDto = new NewPrivateChatDto(alice.getId());
-      String token = authenticateUser(alice);
+      performChatCreationByAlice(alice,
+          status().isBadRequest());
+    }
+
+    private void performChatCreationByAlice(User companion, ResultMatcher... matcher)
+        throws Exception {
+      NewPrivateChatDto content = new NewPrivateChatDto(companion.getId()); //companion
+      String aliceToken = authenticateUser(alice);
 
       mockMvc.perform(
-                      post("/api/chats/private")
-                              .headers(getAuthHeader(token))
-                              .contentType(APPLICATION_JSON)
-                              .content(toJson(newPrivateChatDto)))
-              .andExpect(status().isBadRequest());
+              post(CREATE_PRIVATE_CHAT_PATH)
+                  .headers(getAuthHeader(aliceToken))
+                  .contentType(APPLICATION_JSON)
+                  .content(toJson(content)))
+          .andExpectAll(matcher);
     }
+
   }
 
   @Nested
   class FindMainChat {
 
+    private static final String FIND_MAIN_CHAT_PATH = "/api/v2/country/%s/main-chat";
+
+    private NewPrivateChatDto newPrivateChatDto;
+    private String token;
+
+    @BeforeEach
+    void init() {
+      newPrivateChatDto = new NewPrivateChatDto(alice.getId());
+      token = authenticateUser(alice);
+    }
+
     @ParameterizedTest
     @MethodSource("findMainChatArgs")
     void findMainChat_shouldFindChat(String name, String id) throws Exception {
-      NewPrivateChatDto newPrivateChatDto = new NewPrivateChatDto(alice.getId());
-      String token = authenticateUser(alice);
-
       mockMvc.perform(
-                      get("/api/v2/country/%s/main-chat".formatted(name))
-                              .headers(getAuthHeader(token))
-                              .contentType(APPLICATION_JSON)
-                              .content(toJson(newPrivateChatDto)))
-              .andExpect(status().isOk())
-              .andExpect(jsonPath("id").value(id))
-              .andExpect(jsonPath("name").value(name));
+              get(FIND_MAIN_CHAT_PATH.formatted(name))
+                  .headers(getAuthHeader(token))
+                  .contentType(APPLICATION_JSON)
+                  .content(toJson(newPrivateChatDto)))
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("id").value(id))
+          .andExpect(jsonPath("name").value(name));
     }
 
     @ParameterizedTest
     @MethodSource("findMainChat_shouldThrowArgs")
     void findMainChat_shouldThrow(String countryName) throws Exception {
-      NewPrivateChatDto newPrivateChatDto = new NewPrivateChatDto(alice.getId());
-      String token = authenticateUser(alice);
       mockMvc.perform(
-                      get("/api/v2/country/%s/main-chat".formatted(countryName))
-                              .headers(getAuthHeader(token))
-                              .contentType(APPLICATION_JSON)
-                              .content(toJson(newPrivateChatDto)))
-              .andExpect(status().isNotFound())
-              .andExpect(jsonPath("message").value("Country [%s] not found".formatted(countryName)));
+              get(FIND_MAIN_CHAT_PATH.formatted(countryName))
+                  .headers(getAuthHeader(token))
+                  .contentType(APPLICATION_JSON)
+                  .content(toJson(newPrivateChatDto)))
+          .andExpect(status().isNotFound())
+          .andExpect(jsonPath("message").value("Country [%s] not found".formatted(countryName)));
     }
 
     private static Stream<Arguments> findMainChat_shouldThrowArgs() {
       return Stream.of(
-              Arguments.of("Incorrect"),
-              Arguments.of(" ")
+          Arguments.of("Incorrect"),
+          Arguments.of(" ")
       );
     }
 
@@ -132,39 +148,56 @@ class ChatControllerIntegrationTest extends IntegrationTest {
               Arguments.of("Angola", "3")
       );
     }
-
   }
 
   @Nested
   class UnreadMessages {
 
+    private final NewPrivateChatDto newPrivateChatDto = new NewPrivateChatDto(getAlice().getId());
+    private String token;
+    private final String GET_UNREAD_MESSAGES_PATH = "/api/chats/%s/messages/unread";
+    private final Long CHAT_ID = 10000L;
+
+    @BeforeEach
+    void init() {
+      token = authenticateUser(getAlice());
+    }
+
     @ParameterizedTest
     @MethodSource("getUnreadMessages_shouldThrowArgs")
     void getUnreadMessages_shouldThrow(String chatId, Integer status) throws Exception {
-      NewPrivateChatDto newPrivateChatDto = new NewPrivateChatDto(alice.getId());
-      String token = authenticateUser(alice);
-
       mockMvc.perform(
-                      get("/api/chats/%s/messages/unread".formatted(chatId))
-                              .headers(getAuthHeader(token))
-                              .contentType(APPLICATION_JSON)
-                              .content(toJson(newPrivateChatDto)))
-              .andExpect(status().is(status));
+              get(GET_UNREAD_MESSAGES_PATH.formatted(chatId))
+                  .headers(getAuthHeader(token))
+                  .contentType(APPLICATION_JSON)
+                  .content(toJson(newPrivateChatDto)))
+          .andExpect(status().is(status));
     }
 
     @Test
-    void getUnreadMessages_shouldGetTenMessages() throws Exception {
-      NewPrivateChatDto newPrivateChatDto = new NewPrivateChatDto(alice.getId());
-      String token = authenticateUser(alice);
-      Long chatId = 10000L, expectedLength = 10L;
+    void getUnreadMessages_shouldGetZeroMessages() throws Exception {
+      Long expectedLength = 0L;
+      mockMvc.perform(
+              get(GET_UNREAD_MESSAGES_PATH.formatted(CHAT_ID))
+                  .headers(getAuthHeader(token))
+                  .contentType(APPLICATION_JSON)
+                  .content(toJson(newPrivateChatDto)))
+          .andExpect(jsonPath("$.content").isArray())
+          .andExpect(jsonPath("$.content.length()").value(expectedLength));
+    }
+
+    @Test
+    void getUnreadMessages_shouldGetFourLastMessages() throws Exception {
+      Long expectedLength = 4L;
+      setLastRead(CHAT_ID, alice.getId(), 6L);
 
       mockMvc.perform(
-                      get("/api/chats/%s/messages/unread".formatted(chatId))
-                              .headers(getAuthHeader(token))
-                              .contentType(APPLICATION_JSON)
-                              .content(toJson(newPrivateChatDto)))
-              .andExpect(jsonPath("$.content").isArray())
-              .andExpect(jsonPath("$.content.length()").value(expectedLength));
+              get(GET_UNREAD_MESSAGES_PATH.formatted(CHAT_ID))
+                  .headers(getAuthHeader(token))
+                  .contentType(APPLICATION_JSON)
+                  .content(toJson(newPrivateChatDto)))
+          .andExpect(jsonPath("$.content").isArray())
+          .andExpect(jsonPath("$.content.length()").value(expectedLength));
     }
 
     private static Stream<Arguments> getUnreadMessages_shouldThrowArgs() {
