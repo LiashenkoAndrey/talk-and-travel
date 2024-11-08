22ada496b53d4557a67888df9a20f1422a07a55c
LiashenkoAndrey <a.liashenko@e-u.edu.ua>
2024-11-08 16:12:53 +0200

    add exception handler for async methods

--- src/main/java/online/talkandtravel/config/AsyncConfig.java
+++ src/main/java/online/talkandtravel/config/AsyncConfig.java
@@ -2,18 +2,21 @@ package online.talkandtravel.config;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
+import online.talkandtravel.exception.model.AsyncExceptionHandler;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
 @Configuration
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
 
   @Bean
-  public Executor smartTaskExecutor(
+  public Executor taskExecutor(
       @Value("${taskExecution.corePoolSize}") int corePoolSize,
       @Value("${taskExecution.maxPoolSize}") int maxPoolSize,
       @Value("${taskExecution.queueCapacity}") int queueCapacity
@@ -28,4 +31,12 @@ public class AsyncConfig {
     return executor;
   }
 
+  /**
+   * The {@link AsyncUncaughtExceptionHandler} instance to be used when an exception is thrown
+   * during an asynchronous method execution with {@code void} return type.
+   */
+  @Override
+  public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+    return new AsyncExceptionHandler();
+  }
 }
